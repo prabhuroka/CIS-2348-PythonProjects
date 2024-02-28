cff939eb2ed768bf26d1eeb1004e3f0dc1597302
prabhuroka <75768356+prabhuroka@users.noreply.github.com>
2024-02-28 17:42:10 -0600

    Part c for the code to read from file and then write to another file

--- Homework-2/coding-problem-2.py
+++ Homework-2/coding-problem-2.py
@@ -24,9 +24,23 @@ def read_file(file_name):
                 dates.append(date)
     return dates
 
+
+def write_to_file(input_file_name, output_file_name):
+    dates = read_file(input_file_name)
+
+    with open(output_file_name, 'w') as output_file:
+        for date in dates:
+            output_file.write(date + '\n')
+
+
 if __name__ == '__main__':
     file_name = "inputDates.txt"
     dates_from_file = read_file(file_name)
     print("\nDates from file:")
     for date in dates_from_file:
         print(date)
+    output_file = "parsedDates.txt"
+    write_to_file(file_name, output_file)
+
+    print("\nParsed dates written to 'parsedDates.txt'")
+    
